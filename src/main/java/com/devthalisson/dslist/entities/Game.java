8c61f639ccf2918a43f7b51a3fd0bbfd2a2a5a19
Thalisson <thalissonpop91@gmail.com>
2023-11-14 11:25:23 -0300

    Game SEED

--- src/main/java/com/devthalisson/dslist/entities/Game.java
+++ src/main/java/com/devthalisson/dslist/entities/Game.java
@@ -12,20 +12,29 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {}
 
-    public Game(Long id, String title, Integer year, String genre, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -35,7 +44,7 @@ public class Game {
         return id;
     }
 
-    public void setIg(Long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -85,6 +94,22 @@ public class Game {
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     @Override
