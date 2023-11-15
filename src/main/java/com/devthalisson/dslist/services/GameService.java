3563e77b7d3699ae54a4bb10a60d7a419a2b6237
Thalisson <thalissonpop91@gmail.com>
2023-11-15 19:54:43 -0300

    Create Get games of Lists By Id

--- src/main/java/com/devthalisson/dslist/services/GameService.java
+++ src/main/java/com/devthalisson/dslist/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.devthalisson.dslist.dto.GameDTO;
 import com.devthalisson.dslist.dto.GameMinDTO;
 import com.devthalisson.dslist.entities.Game;
+import com.devthalisson.dslist.projections.GameMinProjection;
 import com.devthalisson.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,14 @@ public class GameService {
         GameDTO dto = new GameDTO(result);
         return dto;
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return  dto;
+    }
+
 
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
